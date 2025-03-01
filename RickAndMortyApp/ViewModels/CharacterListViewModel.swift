707ceee4c43ca49b9028ef41ecccca6396087c6b
tesddev <tesleem.amuda@gmail.com>
2025-03-01 22:57:31 +0100

    add view model code

--- RickAndMortyApp/ViewModels/CharacterListViewModel.swift
+++ RickAndMortyApp/ViewModels/CharacterListViewModel.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+import CharacterKit
+import Combine
+
+class CharacterListViewModel: ObservableObject {
+    @Published var characters: [Character] = []
+    @Published var isLoading = false
+    @Published var errorMessage = ""
+    
+    private let repository: CharacterRepositoryProtocol
+    
+    init(repository: CharacterRepositoryProtocol = CharacterRepository()) {
+        self.repository = repository
+    }
+    
+    func fetchCharacters() {
+        isLoading = true
+        errorMessage = ""
+        
+        repository.fetchCharacters { [weak self] result in
+            DispatchQueue.main.async {
+                guard let self = self else { return }
+                self.isLoading = false
+                
+                switch result {
+                case .success(let characters):
+                    self.characters = characters
+                case .failure(let error):
+                    self.errorMessage = error.localizedDescription
+                }
+            }
+        }
+    }
+}
